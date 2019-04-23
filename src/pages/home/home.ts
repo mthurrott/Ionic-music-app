37bac416cd9d2f3881bc25e916297e6a901ae125
Student <student@sheldon11.istlabs.rit.edu>
2019-04-23 08:42:35 -0400

    FInished the app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,59 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private song: MediaObject;
+  private isSongPaused = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor( private file: File, private media: Media) { }
+    public playMusic() {
+  console.log( 'playMusic called' );
+  console.log( 'path: ' + this.file.dataDirectory );
+  // if song MediaObject doesn't already exist, create a new one
+  try {
+    if ( !this.song ) {
+      console.log('path: ' + this.file.applicationDirectory);
+      let path = (this.file.applicationDirectory + 'www/assets/media/LeanOnMe.mp3' ).replace(/^file:\/\//, '');
+      this.song = this.media.create( path );
+      // this.song = this.media.create( "../../assets/media/LeanOnMe.mp3" );
+      this.song.play();
+    }
+      // if song is paused, don't recreate it; just play it
+    else if ( this.isSongPaused ) {
+      this.song.play();
+      this.isSongPaused = false;
+    }
+  }
+  catch ( e ) {
+    console.log( 'error playing song\n' + e );
+  }
+}
 
+public pauseMusic() {
+  // only try to pause if the song exists
+  if ( this.song ) {
+    this.song.pause();
+    this.isSongPaused = true;
+  }
+}
+
+public stopMusic() {
+  // stop the song, only if it exists
+  if ( this.song ) {
+    this.song.stop();
+    this.song.release(); // frees up the memory
+    this.song = null;
+    this.isSongPaused = false;
+  }
+}
   }
 
-}
+  
+
+
